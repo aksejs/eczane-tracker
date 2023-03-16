6182340e2607933fa735db7630b8f20c9793d298
aksejs <mirukawa@protonmail.ch>
2023-03-16 03:06:24 +0300

    add icons and manifest

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,25 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
+      includeAssets: ['public/favicon.ico', 'public/apple-touch-icon.png'],
+      manifest: {
+        name: 'Eczane Tracker',
+        short_name: 'EczaneTracker',
+        description: 'Duty pharmacies tracker',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'public/assets/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'public/assets/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
     }),
   ],
   server: {
