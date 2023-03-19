5c934931a6c4d029d1f9f456a6b3a1e92bcf5c60
aksejs <mirukawa@protonmail.ch>
2023-03-19 20:19:33 +0300

    update functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,7 +4,9 @@ admin.initializeApp()
 // import parsePharmaciesFunction from './functions/parsePharmacies'
 import searchAddressHttpsFunction from './functions/searchAddressHttps'
 import geocodeAddressHttpsFunction from './functions/geocodeAddressHttps'
+import googleApiEndpointHttpsFunction from './functions/googleApiEndpointHttps'
 
 // export const parsePharmacies = parsePharmaciesFunction
 export const searchAddressHttps = searchAddressHttpsFunction
 export const geocodeAddressHttps = geocodeAddressHttpsFunction
+export const googleApiEndpointHttps = googleApiEndpointHttpsFunction
