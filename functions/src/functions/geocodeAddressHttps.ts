3e5c2472f99542f7d0d825889718ff2eeb9e980a
aksejs <mirukawa@protonmail.ch>
2023-03-20 03:43:11 +0300

    update functiions

--- functions/src/functions/geocodeAddressHttps.ts
+++ functions/src/functions/geocodeAddressHttps.ts
@@ -1,9 +1,10 @@
 import * as functions from 'firebase-functions'
 import axios from 'axios'
 
-export default functions
-  .region('europe-west1')
-  .https.onCall(async ({ lat, lng }) => {
+export default functions.region('europe-west1').https.onCall(async (data) => {
+  const { lat, lng } = data
+
+  try {
     if (!lat || !lng) {
       throw new functions.https.HttpsError(
         'invalid-argument',
@@ -11,8 +12,8 @@ export default functions
       )
     }
 
-    const { data } = await axios.get(
-      `https://maps.googleapis.com/maps/api/geocode/json?`,
+    const { data } = await axios.get<any>(
+      'https://maps.googleapis.com/maps/api/geocode/json',
       {
         params: {
           latlng: `${lat},${lng}`,
@@ -26,4 +27,7 @@ export default functions
     }
 
     return data.results
-  })
+  } catch (e) {
+    throw new functions.https.HttpsError('internal', 'Bad request')
+  }
+})
