5bb5438b9b68dc2b1443e49460cec6d75a9103ad
aksejs <mirukawa@protonmail.ch>
2023-03-29 05:50:54 +0300

    update AddressField logic wip 2

--- src/components/AddressField/AddressField.tsx
+++ src/components/AddressField/AddressField.tsx
@@ -1,6 +1,10 @@
-import { Fragment, useState } from 'react'
+import { Fragment, useEffect, useRef, useState } from 'react'
 import { Combobox, Transition } from '@headlessui/react'
 import { HiChevronUpDown, HiCheckCircle } from 'react-icons/hi2'
+import { useHttpsCallable } from 'react-firebase-hooks/functions'
+import { functions } from '@/config/firebase'
+import { Prediction } from '@/config/types'
+import _ from 'lodash'
 
 const people: Array<{ id: number; name: string }> = [
   { id: 1, name: 'Wade Cooper' },
@@ -11,92 +15,117 @@ const people: Array<{ id: number; name: string }> = [
   { id: 6, name: 'Hellen Schmidt' },
 ]
 
-export default function AddressField() {
-  const [selected, setSelected] = useState(people[0])
+export default function AddressField({
+  defaultValue,
+}: {
+  defaultValue?: string
+}) {
+  const [predictions, setPredictions] = useState<Prediction[]>([])
+  const [executeCallable, loading, error] = useHttpsCallable<any, Prediction[]>(
+    functions,
+    'searchAddressHttps'
+  )
+  const [selected, setSelected] = useState(defaultValue)
   const [query, setQuery] = useState('')
 
-  const filteredPeople =
-    query === ''
-      ? people
-      : people.filter((person) =>
-          person.name
-            .toLowerCase()
-            .replace(/\s+/g, '')
-            .includes(query.toLowerCase().replace(/\s+/g, ''))
-        )
+  const search = async (value: string) => {
+    const resp = await executeCallable({ term: value })
+
+    if (!resp) {
+      return []
+    }
+
+    return resp.data
+  }
+
+  const debouncedSearch = useRef(
+    _.debounce(async (criteria) => {
+      setPredictions(await search(criteria))
+    }, 1000)
+  ).current
 
   return (
-    <div className="flex absolute z-10">
-      <Combobox value={selected} onChange={setSelected}>
-        <div className="relative m-1 w-96">
-          <div className="relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
-            <Combobox.Input
-              className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
-              displayValue={(person: { id: number; name: string }) =>
-                person.name
-              }
-              onChange={(event) => setQuery(event.target.value)}
-            />
-            <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
-              <HiChevronUpDown
-                className="h-5 w-5 text-gray-400"
-                aria-hidden="true"
+    <div className="h-[5vh]">
+      <div className="flex absolute z-10">
+        <Combobox
+          value={selected}
+          onChange={(selectedValue) => {
+            setSelected(selectedValue)
+          }}
+        >
+          <div className="relative m-1 w-96">
+            <div className="relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
+              <Combobox.Input
+                className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
+                displayValue={(prediction: Prediction) =>
+                  prediction.description
+                }
+                onChange={(event) => {
+                  setQuery(event.target.value)
+                  debouncedSearch(event.target.value)
+                }}
               />
-            </Combobox.Button>
-          </div>
-          <Transition
-            as={Fragment}
-            leave="transition ease-in duration-100"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-            afterLeave={() => setQuery('')}
-          >
-            <Combobox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-              {filteredPeople.length === 0 && query !== '' ? (
-                <div className="relative cursor-default select-none py-2 px-4 text-gray-700">
-                  Nothing found.
-                </div>
-              ) : (
-                filteredPeople.map((person) => (
-                  <Combobox.Option
-                    key={person.id}
-                    className={({ active }) =>
-                      `relative cursor-default select-none py-2 pl-10 pr-4 ${
-                        active ? 'bg-teal-600 text-white' : 'text-gray-900'
-                      }`
-                    }
-                    value={person}
-                  >
-                    {({ selected, active }) => (
-                      <>
-                        <span
-                          className={`block truncate ${
-                            selected ? 'font-medium' : 'font-normal'
-                          }`}
-                        >
-                          {person.name}
-                        </span>
-                        {selected ? (
+              <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
+                <HiChevronUpDown
+                  className="h-5 w-5 text-gray-400"
+                  aria-hidden="true"
+                />
+              </Combobox.Button>
+            </div>
+            <Transition
+              as={Fragment}
+              leave="transition ease-in duration-100"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+              afterLeave={() => setQuery('')}
+            >
+              <Combobox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                {predictions.length === 0 && query !== '' ? (
+                  <div className="relative cursor-default select-none py-2 px-4 text-gray-700">
+                    Loading...
+                  </div>
+                ) : (
+                  predictions.map((prediction) => (
+                    <Combobox.Option
+                      key={prediction.place_id}
+                      className={({ active }) =>
+                        `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                          active ? 'bg-teal-600 text-white' : 'text-gray-900'
+                        }`
+                      }
+                      value={prediction}
+                    >
+                      {({ selected, active }) => (
+                        <>
                           <span
-                            className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
-                              active ? 'text-white' : 'text-teal-600'
+                            className={`block truncate ${
+                              selected ? 'font-medium' : 'font-normal'
                             }`}
                           >
-                            <HiCheckCircle
-                              className="h-5 w-5"
-                              aria-hidden="true"
-                            />
+                            {prediction.description}
                           </span>
-                        ) : null}
-                      </>
-                    )}
-                  </Combobox.Option>
-                ))
-              )}
-            </Combobox.Options>
-          </Transition>
-        </div>
-      </Combobox>
+                          {selected ? (
+                            <span
+                              className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                                active ? 'text-white' : 'text-teal-600'
+                              }`}
+                            >
+                              <HiCheckCircle
+                                className="h-5 w-5"
+                                aria-hidden="true"
+                              />
+                            </span>
+                          ) : null}
+                        </>
+                      )}
+                    </Combobox.Option>
+                  ))
+                )}
+              </Combobox.Options>
+            </Transition>
+          </div>
+        </Combobox>
+      </div>
     </div>
   )
 }
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,28 +1,12 @@
-import React, {
-  FunctionComponent,
-  useCallback,
-  useContext,
-  useMemo,
-  useRef,
-  useState,
-} from 'react'
+import { FunctionComponent, useContext } from 'react'
 import _ from 'lodash'
-import {
-  Timestamp,
-  collection,
-  getDocs,
-  query,
-  where,
-} from 'firebase/firestore'
-import { getFunctions } from 'firebase/functions'
-import { useHttpsCallable } from 'react-firebase-hooks/functions'
+import { Timestamp, collection, query, where } from 'firebase/firestore'
+import { useFirestoreQueryData } from '@react-query-firebase/firestore'
 
 import { AddressContext } from '@/store/AddressContext'
-import { Address, Pharmacy, Prediction } from '@/config/types'
-import { app, db } from '@/config/firebase'
+import { Pharmacy } from '@/config/types'
+import { db } from '@/config/firebase'
 import { PharmaciesMap } from '@/features/PharmaciesMap'
-import { useFirestoreQueryData } from '@react-query-firebase/firestore'
-import { Card } from '@/components/Card'
 import AddressField from '@/components/AddressField/AddressField'
 
 function getStartOfToday() {
@@ -30,103 +14,6 @@ function getStartOfToday() {
   now.setHours(0, 0, 0, 0)
   const timestamp = Timestamp.fromDate(now)
   return timestamp // ex. 1631246400
-}
-
-const getPharmacies = async (address: Address) => {
-  const pharmaciesQuery = query(
-    collection(db, 'pharmacies'),
-    where('district', '==', address?.district),
-    where('timestamp', '==', getStartOfToday())
-  )
-
-  const querySnapshot = await getDocs(pharmaciesQuery)
-  let pharmacies: any = []
-
-  querySnapshot.forEach((doc) => {
-    if (doc.data()) {
-      pharmacies.push(doc.data())
-    }
-  })
-
-  if (pharmacies.length) {
-    return pharmacies
-  }
-
-  return null
-}
-
-const AddressInput: React.FC<{ defaultValue?: string }> = ({
-  defaultValue,
-}) => {
-  const [predictions, setPredictions] = useState<Prediction[]>()
-  const [value, setValue] = useState<string>(defaultValue || '')
-  const [executeCallable, loading, error] = useHttpsCallable<any, Prediction[]>(
-    getFunctions(app, 'europe-west1'),
-    'searchAddressHttps'
-  )
-
-  const search = async (value: string) => {
-    const resp = await executeCallable({ term: value })
-
-    if (!resp) {
-      return []
-    }
-
-    return resp.data
-  }
-
-  const debouncedSearch = React.useRef(
-    _.debounce(async (criteria) => {
-      setPredictions(await search(criteria))
-    }, 1000)
-  ).current
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setValue(e.target.value)
-    debouncedSearch(e.target.value)
-  }
-
-  const renderPredictions = () => {
-    if (!predictions?.length || loading || error) {
-      return <React.Fragment />
-    }
-
-    return (
-      <div>
-        {predictions.map((prediction) => (
-          <div
-            onClick={() => {
-              setValue(prediction.description)
-            }}
-            key={prediction.place_id}
-          >
-            {prediction.description}
-          </div>
-        ))}
-      </div>
-    )
-  }
-
-  return (
-    <div>
-      <div>
-        <div>
-          <label htmlFor="address">Address</label>
-          <input
-            id="address"
-            name="address"
-            type="text"
-            placeholder="Your address"
-            onFocus={(e) => e.target.select()}
-            value={value}
-            onChange={handleChange}
-          />
-        </div>
-        <button onClick={() => {}}>Find</button>
-      </div>
-      {renderPredictions()}
-    </div>
-  )
 }
 
 export const MainPage: FunctionComponent = () => {
@@ -144,18 +31,6 @@ export const MainPage: FunctionComponent = () => {
     { enabled: Boolean(district) }
   )
 
-  const renderAddress = () => {
-    if (!address) {
-      return <div>Введите адрес вручную</div>
-    }
-
-    return (
-      <div className="h-[5vh]">
-        <AddressField />
-      </div>
-    )
-  }
-
   const renderMap = () => {
     if (!latLng) {
       return <div>Please pick location</div>
@@ -172,10 +47,8 @@ export const MainPage: FunctionComponent = () => {
 
   return (
     <div className="bg-white dark:bg-slate-800 h-screen dark:text-zinc-300">
-      <>
-        {renderAddress()}
-        {renderMap()}
-      </>
+      {address && <AddressField defaultValue={address} />}
+      {renderMap()}
     </div>
   )
 }
